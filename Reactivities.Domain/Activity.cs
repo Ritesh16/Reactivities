528575da772899f32af1ab7865ddbfdaeccb3ef7
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-04-19 10:48:13 -0400

    Adding structure of app

---
+++ Reactivities.Domain/Activity.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Reactivities.Domain;
+
+public class Activity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string? Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; } 
+    public string Category { get; set; } 
+    public bool IsCancelled { get; set; }
+
+    // location props
+    public string City { get; set; } 
+    public string Venue { get; set; } 
+    public double Latitude { get; set; }
+    
+}
