76e78f8da0a0d230b035aa7a50daf48052e17c69
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-04-20 14:27:18 -0400

    Adding code to connect to database

---
+++ Reactivities.Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Reactivities.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Reactivities.Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
