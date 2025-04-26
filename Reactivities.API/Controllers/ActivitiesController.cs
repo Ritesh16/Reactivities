7c97623ddc38e227e2ef520e44994e249ac810e7
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-04-26 13:24:53 -0400

    Adding code for adding & updating activities

--- Reactivities.API/Controllers/ActivitiesController.cs
+++ Reactivities.API/Controllers/ActivitiesController.cs
@@ -5,6 +5,7 @@ using Reactivities.Domain;
 using Microsoft.EntityFrameworkCore;
 using MediatR;
 using Reactivities.Application.Activities.Queries;
+using Reactivities.Application.Activities.Commands;
 
 namespace Reactivities.API.Controllers
 {
@@ -15,13 +16,26 @@ namespace Reactivities.API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new GetActivityList.Query()); 
+            return await Mediator.Send(new GetActivityList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(string id)
         {
-            return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+            return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity([FromBody] Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> EditActivity([FromBody] Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command { Activity = activity });
+            return NoContent();
         }
     }
 }
