0c83261a1a97d84c804f3183146250118d1b3c84
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-05-14 21:37:33 -0400

    Adding activities filter design

--- Reactivities.Client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities.Client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,15 @@
-import { Grid2, Typography } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>
-        <Typography>Filters will go here</Typography>        
+      <Grid2 size={4}>
+        <ActivityFilters />      
       </Grid2>
     </Grid2>
   )
