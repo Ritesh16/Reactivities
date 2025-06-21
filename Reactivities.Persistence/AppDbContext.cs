8cb1fd8e2a2e77c09c09af74e8428b1beb4c3640
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-06-21 11:11:15 -0400

    Adding setup for ActivityAttendees

--- Reactivities.Persistence/AppDbContext.cs
+++ Reactivities.Persistence/AppDbContext.cs
@@ -7,4 +7,23 @@ namespace Reactivities.Persistence;
 public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Activity> Activities { get; set; }
+    public required DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ActivityAttendee>(x =>
+                    x.HasKey(a => new { a.UserId, a.ActivityId }));
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.Activities)
+            .HasForeignKey(a => a.UserId);
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(x => x.Activity)
+            .WithMany(x => x.Attendees)
+            .HasForeignKey(a => a.ActivityId);
+    }
 }
