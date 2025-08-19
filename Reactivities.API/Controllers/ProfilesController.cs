ab3bb7cdcfdaee78483b8c4fbb92daafe61cf33d
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-08-18 21:21:09 -0400

    Adding code for deleting photo and settingg main photo

--- Reactivities.API/Controllers/ProfilesController.cs
+++ Reactivities.API/Controllers/ProfilesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Reactivities.Application.Profiles.Commands;
@@ -19,7 +20,19 @@ namespace Reactivities.API.Controllers
         [HttpGet("{userId}/photos")]
         public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
         {
-            return HandleResult(await Mediator.Send(new GetProfilePhotos.Query{ UserId = userId}));
+            return HandleResult(await Mediator.Send(new GetProfilePhotos.Query { UserId = userId }));
+        }
+
+        [HttpDelete("{photoId}/photos")]
+        public async Task<ActionResult> DeletePhoto(string photoId)
+        {
+            return HandleResult(await Mediator.Send(new DeletePhoto.Command { PhotoId = photoId }));
+        }
+
+        [HttpPut("{photoId}/setMain")]
+        public async Task<ActionResult> SetMainPhoto(string photoId)
+        {
+            return HandleResult(await Mediator.Send(new SetMainPhoto.Command { PhotoId = photoId }));
         }
     }
 }
