60782e2c65e7832d92ae2f933fe9bffc52a0b76b
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-08-18 21:53:14 -0400

    Adding code to display user profile

--- Reactivities.API/Controllers/ProfilesController.cs
+++ Reactivities.API/Controllers/ProfilesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Reactivities.Application.Profiles.Commands;
+using Reactivities.Application.Profiles.Dtos;
 using Reactivities.Application.Profiles.Queries;
 using Reactivities.Domain;
 
@@ -33,6 +34,12 @@ namespace Reactivities.API.Controllers
         public async Task<ActionResult> SetMainPhoto(string photoId)
         {
             return HandleResult(await Mediator.Send(new SetMainPhoto.Command { PhotoId = photoId }));
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<UserProfile>> GetProfile(string userId)
+        {
+            return HandleResult(await Mediator.Send(new GetProfile.Query { UserId = userId }));
         }
     }
 }
