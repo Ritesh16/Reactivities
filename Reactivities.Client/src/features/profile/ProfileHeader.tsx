62c24dd52bb4d8ec1aa5827685e42db9def5af32
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-08-21 22:30:32 -0400

    Adding code to display profile page & Photos

--- Reactivities.Client/src/features/profile/ProfileHeader.tsx
+++ Reactivities.Client/src/features/profile/ProfileHeader.tsx
@@ -1,18 +1,26 @@
 import { Avatar, Box, Button, Chip, Divider, Grid2, Paper, Stack, Typography } from "@mui/material";
 import { tr } from "date-fns/locale"
 
-export default function ProfileHeader() {
+type Props = {
+  profile: Profile
+}
+
+export default function ProfileHeader({ profile }: Props) {
   const isFollowing = true;
 
   return (
-    <Paper elevation={3} sx={{p:4, borderRadius: 3}}>
+    <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
       <Grid2 container spacing={2}>
         <Grid2 size={8}>
           <Stack direction='row' spacing={3} alignItems='center'>
-            <Avatar sx={{width: 150, height: 150}}/>
+            <Avatar
+              src={profile.imageUrl}
+              alt={profile.displayName + ' image'}
+              sx={{ width: 150, height: 150 }}
+            />
             <Box display='flex' flexDirection='column' gap={2}>
-              <Typography variant="h4">Display Name</Typography>
-              {isFollowing && <Chip variant="outlined" color="secondary" label="Following" sx={{borderRadius:1}}/>}
+              <Typography variant="h4">{profile.displayName}</Typography>
+              {isFollowing && <Chip variant="outlined" color="secondary" label="Following" sx={{ borderRadius: 1 }} />}
             </Box>
           </Stack>
         </Grid2>
@@ -21,18 +29,18 @@ export default function ProfileHeader() {
             <Box display='flex' justifyContent='space-around' width='100%'>
               <Box textAlign='center'>
                 <Typography variant="h6">Followers</Typography>
-                 <Typography variant="h3">5</Typography>
+                <Typography variant="h3">5</Typography>
               </Box>
               <Box textAlign='center'>
                 <Typography variant="h6">Following</Typography>
-                 <Typography variant="h3">42</Typography>
+                <Typography variant="h3">42</Typography>
               </Box>
             </Box>
-            <Divider sx={{width: '100%'}} />
+            <Divider sx={{ width: '100%' }} />
             <Button fullWidth
-            variant="outlined"
-            color={isFollowing ? 'error': 'success'}>
-              {isFollowing? 'Unfollow' : 'Follow'}
+              variant="outlined"
+              color={isFollowing ? 'error' : 'success'}>
+              {isFollowing ? 'Unfollow' : 'Follow'}
             </Button>
           </Stack>
         </Grid2>
