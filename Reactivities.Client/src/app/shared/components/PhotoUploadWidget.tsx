8a7ebadffe21a8810705f796b916a8fb243809f9
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-09-01 21:19:30 -0400

    Adding code to add cropper

--- Reactivities.Client/src/app/shared/components/PhotoUploadWidget.tsx
+++ Reactivities.Client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,17 +1,68 @@
-import { Grid2, Typography } from "@mui/material";
+import { CloudUpload } from "@mui/icons-material";
+import { Box, Grid2, Typography } from "@mui/material";
+import { useCallback, useRef, useState } from "react";
+import { useDropzone } from 'react-dropzone';
+import Cropper, { ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState<object & { preview: string; }[]>([]);
+  const cropperRef = useRef<ReactCropperElement>(null);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    setFiles(acceptedFiles.map(file => Object.assign(file, {
+      preview: URL.createObjectURL(file as Blob)
+    })));
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   return (
     <Grid2 container spacing={3}>
-        <Grid2 size={4}>
-            <Typography variant="overline" color="secondary">Step 1 - Add Photo</Typography>
-        </Grid2>
-        <Grid2 size={4}>
-            <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
-        </Grid2>
-        <Grid2 size={4}>
-            <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
-        </Grid2>
+      <Grid2 size={4}>
+        <Typography variant="overline" color="secondary">Step 1 - Add Photo</Typography>
+        <Box {...getRootProps()}
+          sx={{
+            border: 'dashed 3px #eee',
+            borderColor: isDragActive ? 'green' : '#eee',
+            borderRadius: '5px',
+            paddingTop: '30px',
+            textAlign: 'center',
+            height: '280px'
+          }}
+        >
+          <input {...getInputProps()} />
+          <CloudUpload sx={{ fontSize: 80 }} />
+          <Typography variant="h4">Drop image here</Typography>
+        </Box>
+      </Grid2>
+      <Grid2 size={4}>
+        <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
+        {files[0]?.preview &&
+          <Cropper
+            src={files[0]?.preview}
+            style={{height: 300, width: '90%'}}
+            initialAspectRatio={1}
+            aspectRatio={1}
+            preview='.img-preview'
+            guides={false}
+            viewMode={1}
+            background={false}
+          />
+        }
+      </Grid2>
+      <Grid2 size={4}>
+        {files[0]?.preview && (
+            <>
+              <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
+              <div
+                className="img-preview"
+                style={{width: 300, height: 300, overflow: 'hidden'}}
+              />
+            </>
+        )}
+        
+      </Grid2>
     </Grid2>
   )
 }
