b0c554bfca25a59f0138c0efbf0b99e27f6ab1fb
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-09-07 10:46:13 -0400

    Adding code to delete image

--- Reactivities.Client/src/lib/hooks/useProfile.ts
+++ Reactivities.Client/src/lib/hooks/useProfile.ts
@@ -28,24 +28,24 @@ export const useProfile = (id?: string) => {
             const formData = new FormData();
             formData.append('file', file);
             const response = await agent.post('/profiles/add-photo', formData, {
-                headers: {'Content-Type': 'multipart/form-data'}
+                headers: { 'Content-Type': 'multipart/form-data' }
             });
 
             return response.data;
         },
-        onSuccess: async(photo: Photo) => {
+        onSuccess: async (photo: Photo) => {
             await queryClient.invalidateQueries({
                 queryKey: ['photos', id]
             });
             queryClient.setQueryData(['user'], (data: User) => {
-                if(!data) return data;
+                if (!data) return data;
                 return {
                     ...data,
                     imageUrl: data.imageUrl ?? photo.url
                 }
             });
-             queryClient.setQueryData(['profile', id], (data: Profile) => {
-                if(!data) return data;
+            queryClient.setQueryData(['profile', id], (data: Profile) => {
+                if (!data) return data;
                 return {
                     ...data,
                     imageUrl: data.imageUrl ?? photo.url
@@ -60,18 +60,29 @@ export const useProfile = (id?: string) => {
         },
         onSuccess: (_, photo) => {
             queryClient.setQueryData(['user'], (userData: User) => {
-                if(!userData) return userData;
+                if (!userData) return userData;
                 return {
                     ...userData,
                     imageUrl: photo.url
                 }
             });
             queryClient.setQueryData(['profile', id], (profile: Profile) => {
-                if(!profile) return profile;
+                if (!profile) return profile;
                 return {
                     ...profile,
                     imageUrl: photo.url
                 }
+            })
+        }
+    });
+
+    const deletePhoto = useMutation({
+        mutationFn: async (photoId: string) => {
+            await agent.delete(`/profiles/${photoId}/photos`)
+        },
+        onSuccess: (_, photoId) => {
+            queryClient.setQueryData(['photos', id], (photos: Photo[]) => {
+                return photos?.filter(x => x.id !== photoId)
             })
         }
     });
@@ -87,6 +98,7 @@ export const useProfile = (id?: string) => {
         loadingPhotos,
         isCurrentUser,
         uploadPhoto,
-        setMainPhoto
+        setMainPhoto,
+        deletePhoto
     }
 }
