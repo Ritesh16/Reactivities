186427c0ef8829708f634b0f5d8c14ffe0175d48
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-09-08 21:29:22 -0400

    Adding comment table and migrations

--- Reactivities.Persistence/Migrations/20250908023316_CommentEntityAdded.Designer.cs
+++ Reactivities.Persistence/Migrations/20250908023316_CommentEntityAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reactivities.Persistence;
 
@@ -10,9 +11,11 @@ using Reactivities.Persistence;
 namespace Reactivities.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250908023316_CommentEntityAdded")]
+    partial class CommentEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
